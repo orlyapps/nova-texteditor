4ffe3c7b53a9c46029b4daa51b0ae5309192c5a9
Florian Strauß <fs@orlyapps.de>
2024-02-15 10:18:39 +0100

    “wip”

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,7 +1,7 @@
 <template>
     <PanelItem :index="index" :field="field">
         <template #value>
-           <div class="hundeschule">
+           <div class="hundeschule nietiedt" style="word-break: auto-phrase;">
              <div class="prose max-w-none" v-html="field.displayedAs"></div>
            </div>
         </template>
