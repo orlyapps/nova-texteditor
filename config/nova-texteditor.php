dfc9655c04a97c43e5fb9e930746300708347146
Florian Strauß <fs@orlyapps.de>
2024-04-08 11:28:36 +0200

    “wip”

--- config/nova-texteditor.php
+++ config/nova-texteditor.php
@@ -4,6 +4,7 @@ return [
     'user_class' => \App\Models\User::class,
     'nova_user_class' => \App\Nova\User::class,
     'template_class' => \Orlyapps\NovaTexteditor\Models\Template::class,
+    'template_controller' => \Orlyapps\NovaTexteditor\Http\Controllers\TemplateController::class,
     'categories' => [
         'letter' => 'Letter',
     ],
